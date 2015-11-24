8cb3003a5766fb22e08e731ef53a270aede57772
akrysynsky <driuny@gmail.com>
2015-11-24 00:14:29 +0000

    :text field is dirty by default. add required attribute

--- src/angular-material-design-lite.js
+++ src/angular-material-design-lite.js
@@ -18,13 +18,14 @@
   angular.module('mdl').directive('mdlTextField', function(mdlConfig){
     return {
       restrict: 'E',
-      template: '<div class="mdl-textfield mdl-js-textfield" ng-class="ngClass"><input class="mdl-textfield__input" type="{{type}}" ng-model="ngModel" /><label class="mdl-textfield__label">{{label}}</label></div>',
+      template: '<div class="mdl-textfield mdl-js-textfield is-dirty" ng-class="ngClass"><input class="mdl-textfield__input" type="{{type}}" ng-model="ngModel" /><label class="mdl-textfield__label">{{label}}</label></div>',
       scope: {
         ngModel: '='
       },
       link: function($scope, el, $attrs){
         $scope.label = $attrs.label;
         $scope.type = $attrs.type ? $attrs.type : 'text';
+        $scope.required = $attrs.required ? $attrs.required : 'false';
         $scope.ngClass = {
           'mdl-textfield--floating-label': mdlConfig.floating
         };
